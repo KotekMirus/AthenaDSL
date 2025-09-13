76e78ba4150685769975d55ffa0cd556e199afce
KotekMirus <kozdebamateusz0@gmail.com>
2025-09-13 09:56:55 +0200

    Added enabling/disabling of the button on the website based on the Python loading state, added type hints in the main_web.py file.

--- code/main_web.py
+++ code/main_web.py
@@ -1,23 +1,27 @@
 import tokenizer as tokenizer
 import parser as parser
 import interpreter_generator as interpreter
+from exam_elements_handlers import Exam_Element
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
 import base64
 import js
 
 
-def main(user_input: str):
-    whole_text_tokenized = tokenizer.segment_text(user_input)
-    parsed_document = parser.parse_document(whole_text_tokenized)
-    font_path = "fonts/arial.ttf"
+def main(user_input: str) -> None:
+    whole_text_tokenized: list[list[str]] = tokenizer.segment_text(user_input)
+    parsed_document: dict[str, list[list[Exam_Element]]] = parser.parse_document(
+        whole_text_tokenized
+    )
+    font_path: str = "fonts/arial.ttf"
     pdfmetrics.registerFont(TTFont("Arial", font_path))
-    pdf = interpreter.PDF_Creator("TEST.pdf", parsed_document, "Arial")
+    pdf: interpreter.PDF_Creator = interpreter.PDF_Creator(
+        "TEST.pdf", parsed_document, "Arial"
+    )
     pdf.create_empty_pdf()
     pdf.add_to_pdf()
     pdf.save_pdf()
     with open("TEST.pdf", "rb") as file:
         file_content = file.read()
         base64_encoded = base64.b64encode(file_content).decode("utf-8")
-    print(base64_encoded)
     js.document.setpdfView(base64_encoded)
